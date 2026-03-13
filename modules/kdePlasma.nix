8ce22c8bb953857b2c5f938d3a854056d7cc6073
Pogobanane <aenderboy@gmx.de>
2026-03-13 23:51:42 +0100

    enable kde autologin and auto keychain unlock based on luks

--- modules/kdePlasma.nix
+++ modules/kdePlasma.nix
@@ -17,7 +17,10 @@
     # wallpaper
     # scroll direction
     # color scheme and window decorations
-    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      autoLogin.relogin = true;
+    };
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     hardware.bluetooth.enable = true;
@@ -27,5 +30,34 @@
       nautilus
       openvpn
     ];
+
+    # Fix for LUKS to unlock keyring with auto login. See https://github.com/NixOS/nixpkgs/pull/282317
+    # https://github.com/lf-/dotfiles/commit/4eb47c29c4c1707b4f11989de6cae5de3547c3aa
+    systemd.services.display-manager = {
+      serviceConfig = {
+        KeyringMode = "inherit";
+      };
+    };
+
+    services.displayManager = {
+      autoLogin.enable = true;
+      autoLogin.user = "peter";
+      defaultSession = "plasma";
+    };
+
+    security.pam.services.login.enableKwallet = true;
+    security.pam.services = {
+      sddm-autologin.text = lib.mkForce ''
+        auth requisite ${pkgs.linux-pam}/lib/security/pam_nologin.so
+        auth optional ${config.systemd.package}/lib/security/pam_systemd_loadkey.so
+        auth optional ${pkgs.kdePackages.kwallet-pam}/lib/security/pam_kwallet5.so
+        auth required ${pkgs.linux-pam}/lib/security/pam_succeed_if.so uid >= ${toString config.services.displayManager.sddm.autoLogin.minimumUid} quiet
+        auth required ${pkgs.linux-pam}/lib/security/pam_permit.so
+        account include sddm
+        password include sddm
+        session include sddm
+      '';
+    };
+
   };
 }
