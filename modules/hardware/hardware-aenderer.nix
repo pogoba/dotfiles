e09fe560248a326c9910c30da39b336f65e0684d
Pogobanane <aenderboy@gmx.de>
2026-03-23 23:04:09 +0100

    aenderer: use grub

--- modules/hardware/hardware-aenderer.nix
+++ modules/hardware/hardware-aenderer.nix
@@ -7,8 +7,14 @@
     ./disko-aenderer.nix
   ];
 
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.refind.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.refind.enable = true;
+  # boot.loader.grub.devices = [ "/dev/disk/by-id/nvme-Micron_MTFDKBA512TGD-2BK15ABLT_253652D5E602" ];
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.gfxmodeEfi = "1920x1200";
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" ];
   boot.initrd.kernelModules = [ ];
